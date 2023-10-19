8297b5a223c070d94e2f425c7a6e8682c9899693
Joaomlg <joaomarcos1999jm@gmail.com>
2023-10-18 22:32:25 -0300

    feature: printf using usart2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,6 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
 #include "math.h"
 
 #include "Drivers/l298n_driver.h"
@@ -49,6 +50,8 @@ I2C_HandleTypeDef hi2c1;
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 
+UART_HandleTypeDef huart2;
+
 /* USER CODE BEGIN PV */
 L298N_HandleTypeDef bridge_handler;
 HMC5883L_HandlerTypeDef compass_handler;
@@ -62,7 +65,20 @@ static void MX_GPIO_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
+}
+
 void L298N_Init(void);
 void HMC5883L_Init(void);
 void HMC5883L_Calibrate(void);
@@ -104,6 +120,7 @@ int main(void)
   MX_TIM3_Init();
   MX_TIM2_Init();
   MX_I2C1_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
   L298N_Init();
   HMC5883L_Init();
@@ -126,6 +143,8 @@ int main(void)
 
     float degress = CompassService_GetNormalizedDegressAngle(compass_data.x_axis, compass_data.y_axis,
     		MAGNETIC_DECLINATION, &compass_calibration_params);
+
+    printf("Angle: %d\n\r", (int)degress);
 
     HAL_Delay(200);
     /* USER CODE END WHILE */
@@ -309,6 +328,39 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 2 */
   HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
 
