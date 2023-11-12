fda44bdc837cdb0f34cae7f83c513ff4371ccf43
Mariana Leite <marianaleite785@gmail.com>
2023-11-11 23:59:41 -0300

    Refactor compass service

--- Core/Src/Services/compass_service.c
+++ Core/Src/Services/compass_service.c
@@ -10,53 +10,100 @@
  */
 
 #include "Services/compass_service.h"
+#include "Drivers/hmc5883l_driver.h"
+#include "Services/data_filter_service.h"
 
-void CompassService_InitCalibrationParams(CompassCalibrationParamsTypeDef *calibration_params)
+HMC5883L_HandlerTypeDef magnetometerHandler;
+CompassCalibrationParamsTypeDef calibrationParams;
+
+buffer_t xBuffer;
+buffer_t yBuffer;
+
+float angle;
+
+void CompassService_Init(I2C_HandleTypeDef* hi2c, TIM_HandleTypeDef* htim)
 {
-	calibration_params->xmin = 32767;
-	calibration_params->ymin = 32767;
+	magnetometerHandler.hi2c = hi2c;
+	magnetometerHandler.samples_avg = HMC5883L_8_SAMPLES_AVG;
+	magnetometerHandler.output_rate = HMC5883L_OUTPUT_RATE_75_HZ;
+	magnetometerHandler.measurement_mode = HMC5883L_MEASUREMENT_MODE_NORMAL;
+	magnetometerHandler.gain = HMC5883L_GAIN_4_7_GA;
+	magnetometerHandler.i2c_speed = HMC5883L_I2C_NORMAL_SPEED;
+	magnetometerHandler.operation_mode = HMC5883L_CONTINUOUS_MEASUREMENT_MODE;
 
-	calibration_params->xmax = -32768;
-	calibration_params->ymax = -32768;
+	HMC5883LDriver_Init(&magnetometerHandler, 100);
 
-	calibration_params->x_offset = 0;
-	calibration_params->y_offset = 0;
+	calibrationParams.x_offset = 24;
+	calibrationParams.y_offset = 14;
+	calibrationParams.x_scale = 1.0;
+	calibrationParams.y_scale = (float)138/135;
 
-	calibration_params->x_scale = 1.0;
-	calibration_params->y_scale = 1.0;
+	DataFilterService_InitBuffer(&xBuffer);
+	DataFilterService_InitBuffer(&yBuffer);
+
+	HAL_TIM_Base_Start_IT(htim);
 }
 
-void CompassService_Calibrate(int16_t x_axis, int16_t y_axis, CompassCalibrationParamsTypeDef *calibration_params)
+void CompassService_Calibrate(int iterations)
+{
+	HMC5883L_DataTypeDef data;
+
+	CompassService_InitCalibrationParams(&calibrationParams);
+
+	for (int i = 0; i < iterations; i++) {
+		if(HMC5883LDriver_Read(&magnetometerHandler, &data, 100)) {
+			CompassService_UpdateCalibrationParams(data.x_axis, data.y_axis, &calibrationParams);
+			HAL_Delay(10);
+		}
+	}
+}
+
+void CompassService_InitCalibrationParams(CompassCalibrationParamsTypeDef *calibrationParams)
+{
+	calibrationParams->xmin = 32767;
+	calibrationParams->ymin = 32767;
+
+	calibrationParams->xmax = -32768;
+	calibrationParams->ymax = -32768;
+
+	calibrationParams->x_offset = 0;
+	calibrationParams->y_offset = 0;
+
+	calibrationParams->x_scale = 1.0;
+	calibrationParams->y_scale = 1.0;
+}
+
+void CompassService_UpdateCalibrationParams(int16_t x_axis, int16_t y_axis, CompassCalibrationParamsTypeDef *calibrationParams)
 {
 	int16_t x_var, y_var;
 
-	if (x_axis < calibration_params->xmin) calibration_params->xmin = x_axis;
-	if (x_axis > calibration_params->xmax) calibration_params->xmax = x_axis;
-	if (y_axis < calibration_params->ymin) calibration_params->ymin = y_axis;
-	if (y_axis > calibration_params->ymax) calibration_params->ymax = y_axis;
+	if (x_axis < calibrationParams->xmin) calibrationParams->xmin = x_axis;
+	if (x_axis > calibrationParams->xmax) calibrationParams->xmax = x_axis;
+	if (y_axis < calibrationParams->ymin) calibrationParams->ymin = y_axis;
+	if (y_axis > calibrationParams->ymax) calibrationParams->ymax = y_axis;
 
-	calibration_params->x_offset = (calibration_params->xmax + calibration_params->xmin) / 2;
-	calibration_params->y_offset = (calibration_params->ymax + calibration_params->ymin) / 2;
+	calibrationParams->x_offset = (calibrationParams->xmax + calibrationParams->xmin) / 2;
+	calibrationParams->y_offset = (calibrationParams->ymax + calibrationParams->ymin) / 2;
 
-	x_var = calibration_params->xmax - calibration_params->xmin;
-	y_var = calibration_params->ymax - calibration_params->ymin;
+	x_var = calibrationParams->xmax - calibrationParams->xmin;
+	y_var = calibrationParams->ymax - calibrationParams->ymin;
 
 	if (x_var > y_var)
 	{
-		calibration_params->x_scale = 1.0;
-		calibration_params->y_scale = y_var > 0 ? (float) x_var / y_var : 1.0;
+		calibrationParams->x_scale = 1.0;
+		calibrationParams->y_scale = y_var > 0 ? (float) x_var / y_var : 1.0;
 	}
 	else
 	{
-		calibration_params->y_scale = 1.0;
-		calibration_params->x_scale = x_var > 0 ? (float) y_var / x_var : 1.0;
+		calibrationParams->y_scale = 1.0;
+		calibrationParams->x_scale = x_var > 0 ? (float) y_var / x_var : 1.0;
 	}
 }
 
-float CompassService_GetNormalizedDegressAngle(int16_t x_axis, int16_t y_axis, CompassCalibrationParamsTypeDef *calibration_params)
+float CompassService_CalculateAngle(int16_t x_axis, int16_t y_axis, CompassCalibrationParamsTypeDef *calibrationParams)
 {
-	float x_norm = (x_axis - calibration_params->x_offset) * calibration_params->x_scale;
-	float y_norm = (y_axis - calibration_params->y_offset) * calibration_params->y_scale;
+	float x_norm = (x_axis - calibrationParams->x_offset) * calibrationParams->x_scale;
+	float y_norm = (y_axis - calibrationParams->y_offset) * calibrationParams->y_scale;
 
 	float heading = atan2f(y_norm, x_norm);
 
@@ -76,16 +123,17 @@ float CompassService_GetNormalizedDegressAngle(int16_t x_axis, int16_t y_axis, C
 	return degress;
 }
 
-void CompassService_HMC5883L_Calibrate(HMC5883L_HandlerTypeDef *compass_handler, CompassCalibrationParamsTypeDef *compass_calibration_params, int iterations)
+void CompassService_UpdateCompassAngle()
 {
-	HMC5883L_DataTypeDef compass_data;
-
-	CompassService_InitCalibrationParams(compass_calibration_params);
-
-	for (int i=0; i<iterations; i++)
-	{
-		HMC5883LDriver_Read(compass_handler, &compass_data, 100);
-		CompassService_Calibrate(compass_data.x_axis, compass_data.y_axis, compass_calibration_params);
-		HAL_Delay(10);
+	HMC5883L_DataTypeDef data;
+	if(HMC5883LDriver_Read(&magnetometerHandler, &data, 100)) {
+		float x = DataFilterService_MovingAverage(&xBuffer, data.x_axis);
+		float y = DataFilterService_MovingAverage(&yBuffer, data.y_axis);
+		angle = CompassService_CalculateAngle(x, y, &calibrationParams);
 	}
+}
+
+float CompassService_GetCompassAngle()
+{
+	return angle;
 }
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -24,6 +24,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "Services/location_service.h"
+#include "Services/compass_service.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -244,6 +245,7 @@ void TIM5_IRQHandler(void)
   HAL_TIM_IRQHandler(&htim5);
   /* USER CODE BEGIN TIM5_IRQn 1 */
   LocationService_UpdateLocation();
+  CompassService_UpdateCompassAngle();
   /* USER CODE END TIM5_IRQn 1 */
 }
 
